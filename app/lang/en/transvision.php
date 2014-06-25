5ec4f01f6e1e4a6e6a2665bd5de3cb29ce3f7bc0
Juergen Berg <techadmin@babelzilla.org>
2014-06-25 23:08:23 +0200

    Added language file for Transvision

---
+++ app/lang/en/transvision.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    'case_sensitive' => 'Case sensitive
+',
+    'option' => 'Options',
+    'perfect_match' => 'Perfect match',
+    'placeholder' => 'Type your search here',
+    'repository' => 'Repository',
+    'search' => 'Search',
+    'search_in' => 'Search in',
+    'source_lang' => 'Source language',
+    'target_lang' => 'Target language',
+    'whole_world' => 'Whole world',
+    'searchheadline' => 'Transvision search',
+);
